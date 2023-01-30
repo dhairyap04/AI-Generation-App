28ee208202a00514b76fe097e4f4788cdf9d5814
Dhairya Prajapati <d.prajapati3430@gmail.com>
2023-01-29 22:25:20 -0700

    created an AI-Image-Generation App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,11 @@ import React from "react";
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 import { logo } from "./assets";
-import { Home, CreatePost } from "./pages";
+import { Home, CreatePost } from "./page";
 
-const App = () => {
-  return;
+const App = () => (
   <BrowserRouter>
-    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py border-b border-b[#e6ebf4]">
+    <header className="w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]">
       <Link to="/">
         <img src={logo} alt="logo" className="w-28 object-contain" />
       </Link>
@@ -22,10 +21,10 @@ const App = () => {
     <main className="sm:p-8 px-4 py-8 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="'create-post" element={<CreatePost />} />
+        <Route path="/create-post" element={<CreatePost />} />
       </Routes>
     </main>
-  </BrowserRouter>;
-};
+  </BrowserRouter>
+);
 
 export default App;
